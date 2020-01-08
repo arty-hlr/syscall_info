1d68051ab5e944fe2280d0a4b94e72d01c87f566
Calle Svensson <calle.svensson@zeta-two.com>
2020-01-08 19:20:25 +0100

    Use argparse

--- syscall.py
+++ syscall.py
@@ -5,12 +5,9 @@ import sys
 from os import path
 import subprocess
 import re
+import argparse
 
-if len(sys.argv) < 3:
-    print('Usage: syscall <arch> <syscall>')
-    exit()
-
-infos = {
+ARCHITECTURES = {
     'arm64':('aarch64-linux',('x0','x1','x2','x3','x4','x5'),('svc #0','x8','x0')),
     'x64':('amd64-linux',('rdi','rsi','rdx','r10','r8','r9'),('syscall','rax','rax')),
     'arm':('arm-linux',('r0','r1','r2','r3','r4','r5'),('swi 0x0','r7','r0')),
@@ -24,12 +21,17 @@ infos = {
     'sparc64':('sparc-n-linux',('o0','o1','o2','o3','o4','o5'),('t','0x6d','g1','o0'))
 }
 
-arch = sys.argv[1]
-name = sys.argv[2]
+parser = argparse.ArgumentParser(description='Lookup syscall information.')
+parser.add_argument('arch', help='The target architecture', choices=ARCHITECTURES.keys())
+parser.add_argument('syscall', help='The syscall name/number to look up')
+args = parser.parse_args()
 
-filename = infos[arch][0]
-order = infos[arch][1]
-instruction_sys_ret = infos[arch][2]
+arch = args.arch
+name = args.syscall
+
+filename = ARCHITECTURES[arch][0]
+order = ARCHITECTURES[arch][1]
+instruction_sys_ret = ARCHITECTURES[arch][2]
 filepath = path.join(path.dirname(path.realpath(__file__)), 'syscalls', f'{filename}.xml')
 
 syscall = ET.parse(f'{filepath}').getroot().find(f"./syscall[@name='{name}']").attrib['number']
